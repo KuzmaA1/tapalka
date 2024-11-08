9b7fa08a211e3187d4565cd67da2f1f10f0ffdf8
KuzmaA1 <kuzmaandriy0502@gmail.com>
2024-11-08 22:22:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const https = require('https');
 const fs = require('fs');
 
 var options = {
-    host: 'https://tapalka.onrender.com:3000',
+    host: 'https://tapalka.onrender.com',
+    port: '3000',
     path: ''
 }
 
 https.createServer(options,(req,res)=>{
-        Console.log(req);
+        res.end("1");
 }).listen(3000,(err)=>{
     if(err)throw err;
     console.log('amit gamyak');
