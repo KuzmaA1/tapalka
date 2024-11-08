900425a0a851795e1dd2842e5bc06461573c61fc
KuzmaA1 <kuzmaandriy0502@gmail.com>
2024-11-08 23:12:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((req,res)=>{
+        Console.log(1);
         res.end("1");
 }).listen(3000,(err)=>{
     if(err)throw err;
