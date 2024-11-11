cda087339182fbbeb164a8dfb6454785820b34e4
KuzmaA1 <kuzmaandriy0502@gmail.com>
2024-11-11 21:40:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-//var mysql = require('mysql');
+var mysql = require('mysql2');
 const http = require('http');
 const fs = require('fs');
 
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+con.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the MySQL database.');
+});
+/*
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req,res)=>{
@@ -22,3 +37,4 @@ const server = http.createServer((req,res)=>{
         if(err)throw err;
         console.log('amit gamyak');
 });
+*/
